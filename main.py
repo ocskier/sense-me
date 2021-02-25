f06ca8b25bf4f61a72a74b5a59a77710caf7ab1d
Jon Jackson <ocskier@gmail.com>
2021-02-25 12:53:18 -0500

    feat: working login page with user page

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class LoginPage(Screen):
+    pass
+
+class UserPage(Screen):
+    pass
+
+class SenseMe(ScreenManager):
+    pass
+
+class SenseMeApp(App):
+    def build(self):
+        Builder.load_file(os.path.abspath(os.path.join(os.path.dirname(__file__),'sense-me.kv')))
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(LoginPage(name="login"))
+        screen_manager.add_widget(UserPage(name="user"))
+        screen_manager.current = "login"
+        return screen_manager
+
+
+if __name__ == '__main__':
+    SenseMeApp().run()
