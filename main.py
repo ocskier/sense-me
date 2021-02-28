9893a24894d2e6c9e8f22819fe9507da19be5fe5
Jon Jackson <ocskier@gmail.com>
2021-02-27 23:44:54 -0500

    feat: random color generator on button click

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+from colorutils import random_web
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.utils import get_color_from_hex
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 users = [{
@@ -13,18 +15,24 @@ class LoginPage(Screen):
         super(LoginPage, self).__init__(**kwargs)
 
     def runLogin(self, event):
-        user_inpt = self.ids.userInput.text
-        pass_inpt = self.ids.passInput.text
+        user_input = self.ids.userInput.text
+        pass_input = self.ids.passInput.text
         for user in users:
-            if user["username"] == user_inpt and user["password"] == pass_inpt:
+            if user["username"] == user_input and user["password"] == pass_input:
+                self.ids.alert.size_hint_y = 0
                 print("Logged In")
                 self.parent.current = "user"
             else: 
                 self.ids.alert.size_hint_y = 0.2
-                self.ids.alert.text = "Invalid Credentials!!"
+                self.ids.alertLabel.text = "Invalid Credentials!!"
     pass
 
 class UserPage(Screen):
+    def __init__(self,**kwargs):
+        super(UserPage, self).__init__(**kwargs)
+
+    def getMood(self, event):
+        self.ids.moodBtn.background_color = get_color_from_hex(random_web())
     pass
 
 class SenseMe(ScreenManager):
